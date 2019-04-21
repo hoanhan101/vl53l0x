e36f7e2a77999cfa90437a77a3abd9bf828db07a
Hoanh An <hoanhan@bennington.edu>
2019-04-21 19:07:37 +0000

    add measurement logging

--- vl53l0x/api.py
+++ vl53l0x/api.py
@@ -94,7 +94,6 @@ class VL53L0X(object):
 
     def perform_single_ref_calibration(self, byte):
         self.write_byte(VL53L0X_REG_SYSRANGE_START, VL53L0X_REG_SYSRANGE_MODE_START_STOP | byte)
-
         self.write_byte(VL53L0X_REG_SYSRANGE_START, 0x00)
 
     def ref_calibration_io(self, byte):
@@ -118,7 +117,6 @@ class VL53L0X(object):
         self.write_byte(VL53L0X_REG_POWER_MANAGEMENT_GO1_POWER_FORCE, 0)
 
         self.perform_ref_calibration()
-
         self.perform_ref_signal_measurement()
 
     def perform_ref_signal_measurement(self):
@@ -127,7 +125,6 @@ class VL53L0X(object):
         self.perform_single_ranging_measurement()
 
         self.write_byte(0xFF, 0x01)
-
         self.write_byte(0xFF, 0x00)
 
         # restore static sequence config
@@ -135,7 +132,6 @@ class VL53L0X(object):
 
     def perform_single_ranging_measurement(self):
         self.perform_single_measurement()
-
         self.get_ranging_measurement_data()
 
     def perform_single_measurement(self):
@@ -158,12 +154,14 @@ class VL53L0X(object):
     def get_ranging_measurement_data(self):
         raw_data = self.read_block(0x14)
 
-        # TODO - log these out
         range_millimeter = make_uint16(raw_data[11], raw_data[10])
         signal_rate = make_uint16(raw_data[7], raw_data[6])
         ambient_rate = make_uint16(raw_data[9], raw_data[8])
         effective_spad_rtn_count = make_uint16(raw_data[3], raw_data[2])
         device_range_status = raw_data[0]
+
+        print("range: {}\tsignal rate: {}\tambient rate: {}\tspad count: {}\trange status: {}"
+                .format(range_millimeter, signal_rate, ambient_rate, effective_spad_rtn_count, device_range_status))
 
         # update measurement
         self.measurement = range_millimeter
